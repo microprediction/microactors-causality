27ba652651f89d3224b8ede90794febcec6e9640
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-22 14:47:06 -0500

    Update causality.py
    Explanation of small groups for causality

--- causality.py
+++ causality.py
@@ -3,7 +3,7 @@ import datetime as datetime
 with open('timestamp.txt','a') as f:
    f.write(str(datetime.now())+'\n')
 
-
+# We'll use microprediction.org histories and the tigramite causality library 
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
@@ -16,21 +16,27 @@ from tigramite.independence_tests import ParCorr, GPDC, CMIknn, CMIsymb
 from tigramite.models import LinearMediation, Prediction
 from microprediction import MicroReader
 
-
+# To make interesting causality plots with smaller groups (as a start) we 
+# take a hint from the existence of z2 and z3 streams. If this is completely
+# new, either don't worry or see https://www.microprediction.com/knowledge-center 
 mr = MicroReader()
 z3s = [ name for name in mr.get_stream_names() if 'z3~' in name]
 z2s = [ name for name in mr.get_stream_names() if 'z3~' in name]
-
-# Group greedily using z-streams as a hint  
 groups = dict()
 unique_parents = set()
 for zstream in z3s+z2s:
    parents = [ n+'.json' for n in zstream.split('~')[1:-1] ]
    if not any([ p in unique_parents for p in parents ]):
-      groups[parents[0]]=parents
+      groups[parents[0]]=set(parents)
+   else:
+      for p in parents:
+         if p in groups:
+            for p1 in parents: 
+               groups[p].add(p1)
+            
 with open('groups.csv','w') as f:
    for group in groups:
-      f.write( ','.join([parents[0],len(parents),parents]) )
+      f.write( ','.join([parents[0],len(parents)]+list(parents) ) )
     
      
 
