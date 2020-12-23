fa21c386476cb0c18fa0949f21bd97e63439cdf8
Peter Cotton <peter.cotton@microprediction.com>
2020-12-22 19:08:55 -0500

    blah

--- groups.py
+++ groups.py
@@ -3,17 +3,7 @@ import datetime as datetime
 with open('timestamp.txt','a') as f:
    f.write(str(datetime.now())+'\n')
 
-# We'll use microprediction.org histories and the tigramite causality library 
-import numpy as np
-import matplotlib
-from matplotlib import pyplot as plt
-import sklearn
-import tigramite
-from tigramite import data_processing as pp
-from tigramite import plotting as tp
-from tigramite.pcmci import PCMCI
-from tigramite.independence_tests import ParCorr, GPDC, CMIknn, CMIsymb
-from tigramite.models import LinearMediation, Prediction
+
 from microprediction import MicroReader
 
 mr = MicroReader()
